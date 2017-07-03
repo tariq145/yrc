d67e07ce94b287475667edb1cbf277088eb9acae
tariq145 <tariqfnazar@gmail.com>
2017-07-03 02:52:40 +0200

    changeteam

---
+++ php/form.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$message = $_POST['message'];
+
+$send ="Name: " $first. "\nEmail: " .$email."\nTelepone: ".$telephone."\nMessage: ".$message;
+
+print($send);
+//mail("tariqfnazar@gmail.com","Yahweh Real Concept", $msg, "From: ".$first);
+
+ ?>
